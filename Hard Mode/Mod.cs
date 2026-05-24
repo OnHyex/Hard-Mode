5e356b0c56077f77a68bc06760404070784ae58a
OnHyex <100549656+OnHyex@users.noreply.github.com>
2026-05-24 00:22:33 -0400

    Bumped version and added OnHyex to author list

--- Hard Mode/Mod.cs
+++ Hard Mode/Mod.cs
@@ -4,9 +4,9 @@ namespace Hard_Mode
     public class Mod : PulsarMod
     {
 
-        public override string Version => "1.6.1";
+        public override string Version => "1.6.2";
 
-        public override string Author => "EngBot, Pokegustavo, Mest, Craziness924, 18107, sugarbuzz1";
+        public override string Author => "EngBot, Pokegustavo, Mest, Craziness924, 18107, sugarbuzz1, OnHyex";
 
         public override string ShortDescription => "Makes your game a nightmare";
 
